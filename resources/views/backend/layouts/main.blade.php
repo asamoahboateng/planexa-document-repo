ec2f35d96a0a7de0fb0ce085ab7a1523884f3612
kwame Boateng <kwame@ramsalt.com>
2025-03-20 13:15:33 -0400

    bilding dashboard

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }} | @yield('titile')</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -13,7 +13,7 @@
     <!-- Styles / Scripts -->
     @vite(['resources/css/backend.css', 'resources/js/backend.js'])
 </head>
-<body class="bg-white-500">
+<body class="bg-gray-100">
     <div class="drawer lg:drawer-open">
         <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content flex flex-col">
@@ -32,25 +32,25 @@
                     <a class="btn btn-ghost xl:text-xl">{{ config('app.name') }}</a>
                 </div>
                 <div class="navbar-end">
-                    <div class="dropdown dropdown-end">
-                        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                            <div class="indicator">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
-                                <span class="badge badge-sm indicator-item">8</span>
-                            </div>
-                        </div>
-                        <div
-                            tabindex="0"
-                            class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
-                            <div class="card-body">
-                                <span class="text-lg font-bold">8 Items</span>
-                                <span class="text-info">Subtotal: $999</span>
-                                <div class="card-actions">
-                                    <button class="btn btn-primary btn-block">View cart</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+{{--                    <div class="dropdown dropdown-end">--}}
+{{--                        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">--}}
+{{--                            <div class="indicator">--}}
+{{--                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>--}}
+{{--                                <span class="badge badge-sm indicator-item">8</span>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div--}}
+{{--                            tabindex="0"--}}
+{{--                            class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">--}}
+{{--                            <div class="card-body">--}}
+{{--                                <span class="text-lg font-bold">8 Items</span>--}}
+{{--                                <span class="text-info">Subtotal: $999</span>--}}
+{{--                                <div class="card-actions">--}}
+{{--                                    <button class="btn btn-primary btn-block">View cart</button>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                     <div class="dropdown dropdown-end">
                         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
                             <div class="w-10 rounded-full">
@@ -76,18 +76,13 @@
             </div>
             <!-- Page content here -->
             <div class="m-4" >
-                Content
+                @yield('contents')
+
             </div>
 
         </div>
-        <div class="drawer-side">
-            <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
-            <ul class="menu bg-gray-200 min-h-full w-80 p-4">
-                <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
-            </ul>
-        </div>
+        @include('backend.layouts.parts._sidebar')
+
     </div>
 {{--    <div class="navbar bg-greeb-100 shadow-sm mb-6">--}}
 
--- routes/backend.php
+++ routes/backend.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('backend')->group(function () {
     Route::get('/', function () {
-        return view('backend.layouts.main');
+        return view('backend.dashboard');
     });
 });
 //Route::get('/', function () {
