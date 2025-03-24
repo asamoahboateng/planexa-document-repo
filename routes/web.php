e79a4cf780aa6a7c144f09b4c4697a51838b7653
Kwame Boateng <kwameboateng@Kwame-MacBook-Pro-II.local>
2025-03-24 13:45:19 -0400

    web route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', 'backend');
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+//Route::redirect('/', 'backend');
+Route::get('/', function () {
+    return view('welcome');
+});
